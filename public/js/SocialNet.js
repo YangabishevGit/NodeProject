7d959d7c9fc6ab4580bebde327f0dadf56fbd594
Вячеслав <yangabishev@mail.ru>
2020-10-12 20:56:10 +0300

    Глава_1

--- public/js/SocialNet.js
+++ public/js/SocialNet.js
@@ -6,3 +6,35 @@ define(['views/index'], function(indexView){
         initialize:initialize
     }
 })
+
+define(['router'], function(router) {
+    const initialize = function(){
+        checkLogin(runApplication);
+    };
+
+    const checkLogin = function(callback) {
+        $.ajax("/account/authenticated", {
+            method: "GET",
+            success: function() {
+                return callback(true);
+            },
+            error: function(data) {
+                return callback(false);
+            }
+        });
+    };
+    
+    const runApplication = function(authenticated) {
+        if (!authenticated) {
+            window.location.hash = 'login';
+        } else {
+            window.location.hash = 'index';
+        }
+        Backbone.history.start();
+    };
+    
+    return {
+        initialize: initialize
+}
+
+})
--- public/js/views/index.js
+++ public/js/views/index.js
@@ -9,3 +9,74 @@ define(['text!templates/index.html'], function(indexTemplate){
 
     return new indexVIew;
 });
+
+define(['text!templates/register.html'], function(registerTemplate) {
+    const registerView = Backbone.View.extend({
+        el: $('#content'),
+        events: {
+            "submit form": "register"
+        },
+        register: function() {
+            $.post('/register', {
+                firstName: $('input[name=firstName]').val(),
+                lastName: $('input[name=lastName]').val(),
+                email: $('input[name=email]').val(),
+                password: $('input[name=password]').val(),
+            }, function(data) {
+                console.log(data);
+            });
+            return false;
+        },
+        render: function() {
+            this.$el.html(registerTemplate);
+        }
+    });
+    return registerView;
+});
+
+define(['text!templates/login.html'], function(loginTemplate) {
+const loginView = Backbone.View.extend({
+    el: $('#content'),
+    events: {
+        "submit form": "login"
+    },
+    login: function() {
+        $.post('/login', {
+            email: $('input[name=email]').val(),
+            password: $('input[name=password]').val()
+        }, function(data) {
+            console.log(data);
+        }).error(function(){
+            $("#error").text('Unable to login.');
+            $("#error").slideDown();
+        });
+        return false;
+    },
+    render: function() {
+        this.$el.html(loginTemplate);
+        $("#error").hide();
+    }
+});
+    return loginView;
+});
+
+define(['text!templates/forgotpassword.html'], function(forgotpasswordTemplate) {
+    var forgotpasswordView = Backbone.View.extend({
+        el: $('#content'),
+        events: {
+            "submit form": "password"
+        },
+        password: function() {
+            $.post('/forgotpassword', {
+                email: $('input[name=email]').val()
+            }, function(data) {
+                console.log(data);
+            });
+            return false;
+        },
+        render: function() {
+            this.$el.html(forgotpasswordTemplate);
+        }
+    });
+    return forgotpasswordView;
+});
